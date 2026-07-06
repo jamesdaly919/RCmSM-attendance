eb0c967600479a83dcbce66c778ece73a9d8ce9d
jamesdaly919 <jamesdaly919@gmail.com>
2026-07-06 13:35:15 +0800

    Add files via upload
    updating all for the "I was there" fix

--- src/config.js
+++ src/config.js
@@ -7,7 +7,6 @@
 // It's the long code in the sheet's web address:
 // https://docs.google.com/spreadsheets/d/  <THIS PART>  /edit
 export const SHEET_ID = "1q1PLCsV2ASGMBCvX7EJLvZdGjKgMZdd59Yj7AuBmUaQ";
-export const REPORT_URL = "https://script.google.com/macros/s/AKfycbw4F3Xg2h2pv8jBolj6Y4CtjDBV_p_GNrvQdmpCQYk9rTm3a0r9qO3_9kObglXihyWe-g/exec";
 
 // The names of the tabs in the Google Sheet.
 // Only change these if you rename the tabs in the sheet itself.
@@ -23,7 +22,7 @@ export const TABS = {
 // The "I actually attended" report button posts to this Apps Script
 // Web App URL. Leave "" to hide the feature. See the README section
 // "Turn on attendance reports" for how to get this URL (5 minutes).
-export const REPORT_URL = "";
+export const REPORT_URL = "https://script.google.com/macros/s/AKfycbw4F3Xg2h2pv8jBolj6Y4CtjDBV_p_GNrvQdmpCQYk9rTm3a0r9qO3_9kObglXihyWe-g/exec";
 
 // Fallback values used if the Settings tab is missing a row.
 export const DEFAULTS = {
